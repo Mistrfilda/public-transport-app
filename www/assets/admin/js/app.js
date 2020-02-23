f751c0a867f6d6d7ad8292aa8ca5631b11fb042f
filip <filda.kuchar@seznam.cz>
2020-02-23 13:56:02 +0100

    Add symfony webpack encore, add package.json, init yarn packages, add admin template

---
+++ www/assets/admin/js/app.js
@@ -0,0 +1,8 @@
+import '../scss/app.scss';
+
+import $ from "jquery";
+import 'bootstrap';
+import sbAdmin from "../sbadmin/js/sb-admin-2";'../sbadmin/js/sb-admin-2';
+import naja from 'naja';
+
+document.addEventListener('DOMContentLoaded', naja.initialize.bind(naja));
