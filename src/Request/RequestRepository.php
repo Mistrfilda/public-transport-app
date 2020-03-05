6f32001fe64c703fadf81bb03eddd63c681150c3
filip <filda.kuchar@seznam.cz>
2020-03-06 00:10:32 +0100

    Update requests creation, skip creating of request if request is already pending

--- src/Request/RequestRepository.php
+++ src/Request/RequestRepository.php
@@ -6,6 +6,9 @@ namespace App\Request;
 
 use App\Doctrine\BaseRepository;
 use App\Doctrine\NoEntityFoundException;
+use App\Transport\Prague\DepartureTable\DepartureTable;
+use DateTimeImmutable;
+use Doctrine\ORM\NoResultException;
 use Doctrine\ORM\QueryBuilder;
 
 class RequestRepository extends BaseRepository
@@ -20,6 +23,58 @@ class RequestRepository extends BaseRepository
         }
 
         return $request;
+    }
+
+    public function findLastRequestByType(string $type, DateTimeImmutable $now): ?Request
+    {
+        $qb = $this->createQueryBuilder();
+
+        $qb->andWhere($qb->expr()->eq('request.type', ':type'));
+        $qb->setParameter('type', $type);
+
+        $qb->andWhere($qb->expr()->isNull('request.finishedAt'));
+        $qb->andWhere($qb->expr()->isNull('request.failedAt'));
+
+        $qb->orderBy('request.id', 'desc');
+        $qb->setMaxResults(1);
+
+        $qb->andWhere($qb->expr()->gte('request.createdAt', ':createdAt'));
+        $qb->setParameter('createdAt', $now->modify('- 4 hour'));
+
+        try {
+            return $qb->getQuery()->getSingleResult();
+        } catch (NoResultException $e) {
+            return null;
+        }
+    }
+
+    public function findLastRequestByTypeAndDepartureTable(
+        string $type,
+        DepartureTable $departureTable,
+        DateTimeImmutable $now
+    ): ?Request {
+        $qb = $this->createQueryBuilder();
+
+        $qb->andWhere($qb->expr()->eq('request.type', ':type'));
+        $qb->setParameter('type', $type);
+
+        $qb->andWhere($qb->expr()->isNull('request.finishedAt'));
+        $qb->andWhere($qb->expr()->isNull('request.failedAt'));
+
+        $qb->andWhere($qb->expr()->eq('request.pragueDepartureTable', ':departureTable'));
+        $qb->setParameter('departureTable', $departureTable);
+
+        $qb->orderBy('request.id', 'desc');
+        $qb->setMaxResults(1);
+
+        $qb->andWhere($qb->expr()->gte('request.createdAt', ':createdAt'));
+        $qb->setParameter('createdAt', $now->modify('- 12 hour'));
+
+        try {
+            return $qb->getQuery()->getSingleResult();
+        } catch (NoResultException $e) {
+            return null;
+        }
     }
 
     public function createQueryBuilder(): QueryBuilder
