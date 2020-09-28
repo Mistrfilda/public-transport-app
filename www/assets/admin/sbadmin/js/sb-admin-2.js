7daea1cb36741c3f08e143a03f61d26c407001e7
filip <filda.kuchar@seznam.cz>
2020-09-29 00:18:14 +0200

    yarn + composer update, little fix up for scss color variables

--- www/assets/admin/sbadmin/js/sb-admin-2.js
+++ www/assets/admin/sbadmin/js/sb-admin-2.js
@@ -3,34 +3,26 @@ import $ from "jquery";
 var sbAdmin = $(document).ready(function() {
   "use strict"; // Start of use strict
 
+  toggleNavbar();
   // Toggle the side navigation
   $("#sidebarToggle, #sidebarToggleTop").on('click', function() {
     $("body").toggleClass("sidebar-toggled");
     $(".sidebar").toggleClass("toggled");
     if ($(".sidebar").hasClass("toggled")) {
-      $('.sidebar .collapse').collapse('hide');
+      $('.sidebar .collapse').collapse('show');
     }
   });
 
   // Close any open menu accordions when window is resized below 768px
   $(window).resize(function() {
-    if ($(window).width() < 768) {
-      $('.sidebar .collapse').collapse('hide');
-    }
-    
-    // Toggle the side navigation when window is resized below 480px
-    if ($(window).width() < 480 && !$(".sidebar").hasClass("toggled")) {
-      $("body").addClass("sidebar-toggled");
-      $(".sidebar").addClass("toggled");
-      $('.sidebar .collapse').collapse('hide');
-    }
+    toggleNavbar();
   });
 
   // Prevent the content wrapper from scrolling when the fixed side navigation hovered over
   $('body.fixed-nav .sidebar').on('mousewheel DOMMouseScroll wheel', function(e) {
     if ($(window).width() > 768) {
       var e0 = e.originalEvent,
-        delta = e0.wheelDelta || -e0.detail;
+          delta = e0.wheelDelta || -e0.detail;
       this.scrollTop += (delta < 0 ? 1 : -1) * 30;
       e.preventDefault();
     }
@@ -56,5 +48,21 @@ var sbAdmin = $(document).ready(function() {
   });
 
 }); // End of use strict
+
+function toggleNavbar() {
+  if ($(window).width() < 768) {
+    $(".sidebar").addClass("toggled");
+    $('.sidebar .collapse').collapse('show');
+  } else {
+    $(".sidebar").removeClass("toggled");
+  }
+
+  // Toggle the side navigation when window is resized below 480px
+  if ($(window).width() < 480) {
+    $("body").addClass("sidebar-toggled");
+    $(".sidebar").addClass("toggled");
+    $('.sidebar .collapse').collapse('show');
+  }
+}
 
 export default sbAdmin;
