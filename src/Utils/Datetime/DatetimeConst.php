b9fe527431732e971152d090afb6c32bdfe256cb
filip <filda.kuchar@seznam.cz>
2020-12-13 02:02:57 +0100

    Start refactoring to custom datetime library

---
+++ src/Utils/Datetime/DatetimeConst.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types = 1);
+
+
+namespace App\Utils\Datetime;
+
+
+class DatetimeConst
+{
+	public const DEPARTURE_TABLE_DATETIME_FORMAT = 'd. m. Y H:i:s';
+
+	public const DEFAULT_NULL_DATETIME_PLACEHOLDER = '---';
+}
