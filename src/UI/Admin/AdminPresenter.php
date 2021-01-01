1abf56e9ffd5610cddc49618ecc4ea66c4c51707
filip <filda.kuchar@seznam.cz>
2021-01-01 23:58:47 +0100

    make base presenter more abstract, move admin modal things into admin presenter

--- src/UI/Admin/AdminPresenter.php
+++ src/UI/Admin/AdminPresenter.php
@@ -5,12 +5,66 @@ declare(strict_types=1);
 namespace App\UI\Admin;
 
 use App\Admin\CurrentAppAdminGetter;
+use App\UI\Admin\Control\Modal\ModalRendererControl;
+use App\UI\Admin\Control\Modal\ModalRendererControlFactory;
 use App\UI\Admin\Menu\AdminMenuBuilder;
 use App\UI\Shared\BasePresenter;
+use App\UI\Shared\LogicException;
+use Nette\Utils\IHtmlString;
 
 abstract class AdminPresenter extends BasePresenter
 {
+	/** @var string */
+	public const DEFAULT_MODAL_COMPONENT_NAME = 'modalRendererControl';
+
 	protected CurrentAppAdminGetter $currentAppAdminGetter;
+
+	protected ModalRendererControlFactory $modalRendererControlFactory;
+
+	private ?string $modalComponentName = null;
+
+	public function injectModalRendererControlFactory(ModalRendererControlFactory $modalRendererControlFactory): void
+	{
+		$this->modalRendererControlFactory = $modalRendererControlFactory;
+	}
+
+	/**
+	 * @param mixed[] $additionalParameters
+	 * @throws LogicException
+	 */
+	public function showModal(
+		string $componentName = self::DEFAULT_MODAL_COMPONENT_NAME,
+		?string $heading = null,
+		?IHtmlString $content = null,
+		array $additionalParameters = [],
+		?string $templateFile = null
+	): void {
+		$modalComponent = $this->getComponent($componentName);
+		if (! $modalComponent instanceof ModalRendererControl) {
+			throw new LogicException(sprintf(
+				'Component %s is not instance of %s',
+				$componentName,
+				ModalRendererControl::class
+			));
+		}
+
+		$modalComponent->setParameters($heading, $content, $additionalParameters);
+
+		if ($templateFile !== null) {
+			$modalComponent->setTemplateFile($templateFile);
+		}
+
+		$this->modalComponentName = $componentName;
+
+		$this->payload->showModal = true;
+		$this->payload->modalId = $modalComponent->getModalId();
+		$this->redrawControl('modalComponentSnippet');
+	}
+
+	public function getModalComponentName(): ?string
+	{
+		return $this->modalComponentName;
+	}
 
 	public function injectCurrentAppAdminGetter(CurrentAppAdminGetter $currentAppAdminGetter): void
 	{
@@ -40,5 +94,10 @@ abstract class AdminPresenter extends BasePresenter
 	{
 		$this->currentAppAdminGetter->logout();
 		$this->redirect('this');
+	}
+
+	protected function createComponentModalRendererControl(): ModalRendererControl
+	{
+		return $this->modalRendererControlFactory->create();
 	}
 }
