271920e6afa09c9dc348d56fe085f258768f18c2
filip <filda.kuchar@seznam.cz>
2021-01-04 00:19:54 +0100

    finish up refactoring transport restrictions

--- src/UI/Front/Prague/PragueTransportRestriction/Control/Modal/PragueRestrictionModalControlFactory.php
+++ src/UI/Front/Prague/PragueTransportRestriction/Control/Modal/PragueRestrictionModalControlFactory.php
@@ -1,13 +1,8 @@
 <?php
 
-declare(strict_types = 1);
-
+declare(strict_types=1);
 
 namespace App\UI\Front\Prague\PragueTransportRestriction\Control\Modal;
-
-
-use Ramsey\Uuid\UuidInterface;
-
 
 interface PragueRestrictionModalControlFactory
 {
